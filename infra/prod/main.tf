b9e01b4db305ef609bdab8323e2026ce434e985d
Jay Bhavsar <jbhv12@gmail.com>
2023-12-07 13:16:07 +0530

    password token manual auth snap

--- infra/prod/main.tf
+++ infra/prod/main.tf
@@ -2,11 +2,12 @@ module "capstone" {
   source = "../module"
   env    = var.env
   gcp_project_id = var.gcp_project_id
+  gcp_public_resources_bucket = var.gcp_public_resources_bucket
 
   chainlit_auth_secret = var.chainlit_auth_secret
   chainlit_api_key = var.chainlit_api_key
-  aws_cognito_user_pool_id = var.aws_cognito_user_pool_id
-  aws_cognito_client_id = var.aws_cognito_client_id
+  COGNITO_USER_POOL_ID = var.COGNITO_USER_POOL_ID
+  COGNITO_CLIENT_ID = var.COGNITO_CLIENT_ID
   openai_api_key = var.openai_api_key
   aws_access_key_id = var.aws_access_key_id
   aws_secret_access_key = var.aws_secret_access_key
