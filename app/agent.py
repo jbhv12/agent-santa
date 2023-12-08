07e40b2b5391c01f637a6e945bd59b766a6a18d5
Jay Bhavsar <jbhv12@gmail.com>
2023-12-08 16:16:35 +0530

    agent prompt and chars

--- app/agent.py
+++ app/agent.py
@@ -5,27 +5,30 @@ from langchain.chat_models import ChatOpenAI
 from langchain.tools.ddg_search import DuckDuckGoSearchRun
 from langchain.agents import AgentType, Tool, initialize_agent
 from langchain.prompts import MessagesPlaceholder
-from langchain_core.messages import (
-    BaseMessage,
-    SystemMessage,
-)
+from langchain_core.messages import SystemMessage
 from langchain.memory import ConversationBufferMemory, RedisChatMessageHistory
+from constants import *
+import random
 
-
-def get_agent(session_id):
-    SYSTEM_PROMPT=r'''You are now chatting with Santa Claus, the jolly man from the North Pole known for spreading Christmas cheer and love. You love talking about Christmas, the joy of giving, and the magic of the holiday season. Always stay in character as Santa, being joyful, cheerful, and fun. When you don't know the answer to a question, respond in character by saying something like, 'Ho ho ho! That's a wonderful question, but even Santa doesn't know everything!' Always keep the responses in the spirit of Christmas, positive, and family-friendly.
+DEFAULT_PROMPT=r"""You are now chatting with Santa Claus, the jolly man from the North Pole known for spreading Christmas cheer and love. You love talking about Christmas, the joy of giving, and the magic of the holiday season. Always stay in character as Santa, being joyful, cheerful, and fun. When you don"t know the answer to a question, respond in character by saying something like, "Ho ho ho! That"s a wonderful question, but even Santa doesn"t know everything!" Always keep the responses in the spirit of Christmas, positive, and family-friendly.
 Behavior Guidelines for the AI:
-1. Maintain Santa's cheerful and jovial tone in all interactions.
+1. Maintain Santa"s cheerful and jovial tone in all interactions.
 2. Use Christmas-themed language and references in responses.
 3. When unsure about an answer, respond in a Santa-like way, admitting lack of knowledge without breaking character.
 4. Keep all interactions family-friendly, positive, and in the spirit of the holiday season.
-5. Avoid making up information or 'hallucinating'. Be honest and straightforward in a manner fitting Santa's character.
-    '''
+5. Avoid making up information or "hallucinating". Be honest and straightforward in a manner fitting Santa"s character.
+"""
+
+def get_agent(session_id, personality="Santa"):
+
     llm = ChatOpenAI(streaming=True, temperature=0, model="gpt-3.5-turbo-0613")
     search = DuckDuckGoSearchRun()  # SerpAPIWrapper()
     llm_math_chain = LLMMathChain.from_llm(llm=llm, verbose=True)
     # db = SQLDatabase.from_uri("sqlite:///../../../../../notebooks/Chinook.db")
     # db_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True)
+
+
+
     tools = [
         Tool(
             name="Search",
@@ -43,18 +46,21 @@ Behavior Guidelines for the AI:
         #     description="useful for when you need to answer questions about FooBar. Input should be in the form of a question containing full context",
         # ),
     ]
-
+    system_prompt = DEFAULT_PROMPT
+    for character in characters:
+        if character["name"] == personality:
+            system_prompt = character["prompt"]
     agent_kwargs = {
         "input": lambda x: x["input"],
         "extra_prompt_messages": [MessagesPlaceholder(variable_name="memory")],
-        "system_message": SystemMessage(content=SYSTEM_PROMPT),
+        "system_message": SystemMessage(content=system_prompt),
     }
 
-    redis_host = os.environ.get('REDIS_HOST', None)
-    redis_port = os.environ.get('REDIS_PORT', '6379')
-    redis_db = os.environ.get('REDIS_DB', '0')
+    redis_host = os.environ.get("REDIS_HOST", None)
+    redis_port = os.environ.get("REDIS_PORT", "6379")
+    redis_db = os.environ.get("REDIS_DB", "0")
     try:
-        redis_ttl = int(os.environ.get('REDIS_TTL', 600))
+        redis_ttl = int(os.environ.get("REDIS_TTL", 600))
     except:
         redis_ttl = 600
 
@@ -64,8 +70,7 @@ Behavior Guidelines for the AI:
     else:
         memory = ConversationBufferMemory(memory_key="memory", return_messages=True)
 
-
-    agent = initialize_agent(
+    return initialize_agent(
         tools,
         llm,
         agent=AgentType.OPENAI_FUNCTIONS,
@@ -73,6 +78,10 @@ Behavior Guidelines for the AI:
         agent_kwargs=agent_kwargs,
         memory=memory,
     )
-    # agent.run(input="How many people live in canada?")
-    return agent
 
+
+def get_welcome_message(character):
+    for c in characters:
+        if c["name"] == character:
+            return random.choice(c.get("welcome_messages"))
+    return "How can I help today?"
